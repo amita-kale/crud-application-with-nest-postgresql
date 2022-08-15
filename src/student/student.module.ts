e892b9f883b4deb908a4784fce9d0dedfe193ae5
amita-kale <amitakale297@gmail.com>
2022-08-15 23:06:03 +0530

    feat:written interface code in post interface file and defined that entity in entity file

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
-import { StudentController } from './student.controller';
-import { StudentService } from './student.service';
+import {
+    Module
+}
+from '@nestjs/common';
+import {
+    TypeOrmModule
+}
+from '@nestjs/typeorm';
+import {
+    StudentPostEntity
+}
+from './models/post.entity';
+import {
+    StudentController
+}
+from './student.controller';
+import {
+    StudentService
+}
+from './student.service';
 
-@Module({
-  controllers: [StudentController],
-  providers: [StudentService]
-})
-export class StudentModule {}
+@Module({ imports:[TypeOrmModule.forFeature([StudentPostEntity]) ], controllers: [StudentController], providers: [StudentService] }) export class StudentModule {}

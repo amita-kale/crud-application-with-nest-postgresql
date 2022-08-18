129430966261e5912f2a0b214368d12438f0f98d
amita-kale <amitakale297@gmail.com>
2022-08-18 14:56:13 +0530

    added swagger for testing

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import {
 }
 from '@nestjs/core';
 import {
+    DocumentBuilder, SwaggerModule
+}
+from '@nestjs/swagger';
+import {
     AppModule
 }
 from './app.module';
@@ -15,6 +19,9 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe({ disableErrorMessages:false, whitelist:true, forbidNonWhitelisted:true }));
+    const config = new DocumentBuilder() .setTitle('Cats example') .setDescription('The cats API description') .setVersion('1.0') .addTag('cats') .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();

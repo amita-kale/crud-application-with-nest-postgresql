11dcd22acd9472a0243b0a0899bd071854a4507b
dimplejha <dimplejha98@gmail.com>
2022-08-18 16:55:15 +0530

    fix: database string issue resolved and all code is working perfect

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import {
     AppService
 }
 from './app.service';
+import { BookModule } from './book/book.module';
 // import {
 //     BookModule
 // }
@@ -47,5 +48,5 @@ import {
 }
 from './usermodule/users.module';
 
-@Module({ imports: [StudentModule, ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot({ type: 'postgres', host: process.env.DB_HOST, port: parseInt(<string > process.env.DB_PORT), username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE, autoLoadEntities: true, synchronize: true, }), FeedModule, ProductModule, UsersModule, ], controllers: [AppController], providers: [AppService], }) export class AppModule {}
+@Module({ imports: [StudentModule, BookModule, ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot({ type: 'postgres', host: process.env.DB_HOST, port: parseInt(<string > process.env.DB_PORT), username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE, autoLoadEntities: true, synchronize: true, }), FeedModule, ProductModule, UsersModule, ], controllers: [AppController], providers: [AppService], }) export class AppModule {}
 // FlightMModule, BookModule,

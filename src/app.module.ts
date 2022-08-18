af7307f02d496f254014882f7853ec2800d7eece
amita-kale <amitakale297@gmail.com>
2022-08-18 11:26:35 +0530

    change user to userName in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,4 +23,4 @@ import {
 }
 from './student/student.module';
 
-@Module({ imports: [StudentModule, ConfigModule.forRoot({isGlobal:true}), TypeOrmModule.forRoot({ type: 'postgres', host: process.env.POSTGRES_HOST, port: parseInt(<string > process.env.POSTGRES_PORT), username: process.env.POSTGRES_USER, password: process.env.POSTGRES_PASSWORD, database: process.env.POSTGRES_DATABASE, autoLoadEntities: true, synchronize: true, }), ], controllers: [AppController], providers: [AppService], }) export class AppModule {}
+@Module({ imports: [StudentModule, ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot({ type: 'postgres', host: process.env.DB_HOST, port: parseInt(<string > process.env.DB_PORT), username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE, autoLoadEntities: true, synchronize: true, }), ], controllers: [AppController], providers: [AppService], }) export class AppModule {}

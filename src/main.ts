0481b74e924de3ca1d447dd7a52fb030d228c339
amita-kale <amitakale297@gmail.com>
2022-08-22 10:44:31 +0530

    feat: image path stored in  DB successfully

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,22 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    disableErrorMessages:true,
-    whitelist:true,
-    forbidNonWhitelisted:true,
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('swagger example')
     .setDescription('The swagger API description')
     .setVersion('1.0')
     .addTag('swagger')
-    .build(); 
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();

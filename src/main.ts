3fd828c01c45d0c6b6d8646933f992bf05c7f855
dimplejha <dimplejha98@gmail.com>
2022-08-25 11:11:22 +0530

    fix:main issue resolved

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+        disableErrorMessages:false,
+        whitelist:true,
+        forbidNonWhitelisted:true
+      }));
+  const config = new DocumentBuilder()
+    .setTitle('Book Management')
+    .setDescription('Add show Edit Delete -> Book data')
+    .setVersion('1.0')
+    .addTag('Group A Assignment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(3001);
+}
+bootstrap();
